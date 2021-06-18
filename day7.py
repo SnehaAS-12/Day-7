81b8d9877a335dbebc0eb10481a43376b4ee90e2
SnehaAS-12 <85794682+SnehaAS-12@users.noreply.github.com>
2021-06-18 14:21:03 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,18 @@
+def math(a,b):
+          print("Addition of two numbers :",a+b)
+          print("Subtraction of two numbers :",a-b)
+          print("Multplication of two numbers :",a*b)
+          print("Division of two numbers :",a/b)
+a=float(input("Enter the first value : "))
+b=float(input("Enter the second value : "))
+math(a,b)
+
+
+def covid(PatientName,BodyTemperature):
+          if BodyTemperature < str(0) :
+                default=str(98)
+                print("Patient Name is ",PatientName," Body Temperature is ",default)
+          else:
+               print("Patient Name is ",PatientName," Body Temperature is ",BodyTemperature)
+covid("Sneha","") 
+covid("Sneha","98")
